f3ab42a038e72587a4bdc0d71e686c70ca69ea5f
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2024-11-06 22:58:18 +0530

    greatest number in list

---
+++ bca 1st sem/pract13.py
@@ -0,0 +1,16 @@
+# sum of list items
+
+a=[1,2,3,4,5]
+# using while loop
+# i=0
+# b=0
+# while(i<5):
+#     b=b+(a[i])
+#     i=i+1
+# print(b)    
+
+# using for loop
+b=0
+for i in a:
+    b=b+i
+print(b)    
---
+++ bca 1st sem/pract14.py
@@ -0,0 +1,15 @@
+# greatest number in the list
+
+a=[2,1,3,5,8,9]
+i=0
+b=0
+c=len(a)
+d=0
+while(i<c):
+    d=a[i]
+    if(b>d):
+        b=b
+    elif(d>b):
+        b=d
+    i=i+1    
+print(b)    
