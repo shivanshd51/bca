921729064d8146520e04c48533a06b9835cc3c4d
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2024-11-17 11:59:04 +0530

    check3digitnumber

---
+++ bca 1st sem/pract28.py
@@ -0,0 +1,7 @@
+# check number is 3 digit or not 
+a=input("enter the number you want to check :")
+b=len(a)
+if b==3:
+    print('Yes, the number is three digit :')
+else:
+    print('No, the number is not three digit :')
---
+++ bca 1st sem/pract29.py
@@ -0,0 +1,11 @@
+a=int(input("enter the number to check :"))
+#x=range(100,1000,1)
+# if(a in x):
+#     print("yes the number is of three digit")
+# else:
+#     print("no the number is not three digit")    
+
+if(a>99 and a<1000):
+    print("yes the number is of three digit")
+else:
+    print("no the number is not three digit")    
