946ab99990b51f27cb1b8c5f1c6fc379fec534b0
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2024-11-23 13:15:07 +0530

    str

---
+++ bca 1st sem/pract30.py
@@ -0,0 +1,14 @@
+#string datatype
+
+s1="shivansH"
+
+# print(s1.index("s"))
+
+# print(s1.startswith("sh"))
+# print(s1.endswith("sh"))
+
+s2="123"
+# print(s2.isdigit())
+
+# print(s1.islower())
+# print(s1.isupper())
