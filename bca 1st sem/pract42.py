cb5aa097b32412de6d6ffcbd2f2cd55124f28610
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2024-12-08 22:44:02 +0530

    BCA CT1 question

---
+++ bca 1st sem/pract42.py
@@ -0,0 +1,9 @@
+# ct ques 1 
+# take input from user and and check if all the vowels are there or not
+
+a=input("enter the string to check :")
+
+if(("a","A" in a) and ("e","E" in a) and ("i","I" in a) and ("o","O" in a) and ("u","U" in a)):
+    print("yes all the vowels are in",a)
+else:
+    print("no all the vowels are not in",a)    
