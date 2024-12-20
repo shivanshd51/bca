26a513d66ea81a6d99019eb08f3280a73da8bbab
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2024-12-20 22:19:23 +0530

    10 practice program

---
+++ bca 1st sem/pract43.py
@@ -0,0 +1,71 @@
+# 1.adding 2 numbers
+# a=2
+# b=3
+# print(a+b)
+
+# 2.hello world program
+# print("hello world")
+
+# 3.square root program
+# a=int(input("enter the number you want to find square root of :"))
+# print("square root of",a,"is",a**2)
+
+# 4.area of tringle
+# a=int(input("enter the base of triangle :"))
+# b=int(input("enter the height of triangle :"))
+# print("area of triangle is :",1/2*a*b)
+
+# 5.swap 2 variables
+# a=int(input("enter the number to store in var A :"))
+# b=int(input("enter the number to store in var B:"))
+# c=a
+# d=b
+# a=d
+# b=c
+# print("A=",a,"B=",b)
+
+# 6.KM to miles converter
+# a=int(input("enter the distance in kilometer :"))
+# b=a/0.621
+# print("In",a,"kilometers there is",b,"miles")
+
+# 7.check +ve -ve or 0
+# a=int(input("enter the number :"))
+# if a>0:
+#     print("the number is positive..")
+# elif a<0:
+#     print("the number is negative..")
+# else:
+#     print("the number is zero..")
+
+# 8.even/odd
+# a=int(input("enter the number :"))
+# if a%2==0:
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+
+# 9.check leap year
+# a=int(input("enter the year :"))
+# b=a%4
+# if b==0:
+#     print("it is a leap year..")
+# else:
+#     print("it is not a leap year..")
+
+# # 10.largest among three number
+# a=int(input("enter the first number :"))
+# b=int(input("enter the second number :"))
+# c=int(input("enter the third number :"))
+# if a>b>c:
+#     print(a,"is the greatest")
+# if a>c>b:
+#     print(a,"is the greatest")
+# if b>a>c:
+#     print(b,"is the greatest")
+# if b>c>a:
+#     print(b,"is the greatest")
+# if c>a>b:
+#     print(c,"is the greatest")
+# if c>b>a:
+#     print(c,"is the greatest")
