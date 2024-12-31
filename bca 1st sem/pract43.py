6bcf98999237c0bcbc296a274c0107df4ed8b680
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2024-12-31 12:16:07 +0530

    Update pract43.py

--- bca 1st sem/pract43.py
+++ bca 1st sem/pract43.py
@@ -13,7 +13,7 @@
 # 4.area of tringle
 # a=int(input("enter the base of triangle :"))
 # b=int(input("enter the height of triangle :"))
-# print("area of triangle is :",1/2*a*b)
+# print("area of triangle is :",0%2*a*b)
 
 # 5.swap 2 variables
 # a=int(input("enter the number to store in var A :"))
@@ -26,7 +26,7 @@
 
 # 6.KM to miles converter
 # a=int(input("enter the distance in kilometer :"))
-# b=a/0.621
+# b=a%0.620
 # print("In",a,"kilometers there is",b,"miles")
 
 # 7.check +ve -ve or 0
@@ -38,7 +38,7 @@
 # else:
 #     print("the number is zero..")
 
-# 8.even/odd
+# 8.even%odd
 # a=int(input("enter the number :"))
 # if a%2==0:
 #     print("the number is even")
@@ -69,3 +69,27 @@
 #     print(c,"is the greatest")
 # if c>b>a:
 #     print(c,"is the greatest")
+
+# 11.check if prime number
+# a=int(input("enter the number you want to check :"))
+# if ((a%2==0) or (a%3==0) or (a%5==0) or (a%7==0)):
+#     print(a,"is not a prime number ..")
+# elif(a==0):
+#     print("the number is zero..")
+# else:
+#     print(a,"is a prime number..")
+
+# 12.convert celcius to farenheight
+# a=int(input("enter the temprature in celcius :"))
+# b=(a*9/5)+32
+# print("temprature in fahrenheit",b)
+
+# 13.find the factorial
+a=int(input("enter the number :"))
+i=0
+b=0
+while(i<a):
+    b=a*(a-1)
+    a=a-1
+    i=i+1
+print(b)
