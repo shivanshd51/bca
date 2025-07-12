1f8127fc619a0af8213e342a751762d5a6fbe884
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2025-07-12 14:24:36 +0530

    py

---
+++ bca 2nd sem/python/file2.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+ar1=np.array([[1,2,3,4],[4,3,2,1],[2,4,6,8]])
+print(ar1,"\nMatrix A\n")
+ar2=np.array([[1,2,3,4],[4,3,2,1],[2,4,6,8]])
+print(ar2,"\nMatrix B\n")
+
+print(ar1+ar2)
+'''
+ar3=np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+#print(ar.ndim)
+
+for i in range(0,3):
+	for j in range(0,4):
+		ar3[i][j]=ar1[i][j]+ar2[i][j]
+
+print(ar3,"\nMatrix C\n")
+
+
+
+arr1=np.array([1,2,3,5])
+arr1[3]=4
+print(arr1)
+
+'''
