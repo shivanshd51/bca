011cc34a6bfeee9feb1bd5678ead3bef2180a5e7
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2025-08-26 00:29:24 +0530

    loops

---
+++ bca 3rd sem/java/loop.java
@@ -0,0 +1,24 @@
+public class loop {
+    public static void main(String[] args) {
+        // loops
+
+        // printing number from 1-100 using for loop
+        // for(int i = 1;i<=100;i=i+1){
+        //     System.out.println(i);
+        // }
+
+        // printing 100-1 using while loop
+        // int j = 100;
+        // while (j>=1){
+        //     System.out.println(j);
+        //     j=j-1;
+        // }
+
+        // printing 100-1 using do while loop
+        int k = 100;
+        do{
+            System.out.println(k);
+            k=k-1;
+        }while(k>=1);
+    }
+}
