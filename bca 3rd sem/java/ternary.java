914cf89906d6ea9bc4cf5bbb9a01652274ade8c0
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2025-08-26 11:43:39 +0530

    ternary operator

---
+++ bca 3rd sem/java/ternary.java
@@ -0,0 +1,10 @@
+public class ternary {
+    public static void main(String[] args) {
+        int num = 10;
+        int result;
+        result = num/2==4? 10:20;
+        System.out.println(result);
+
+
+    }
+}
