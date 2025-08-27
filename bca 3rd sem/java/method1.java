f4326f455e6c34e19157451143a42fffca42661b
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2025-08-28 00:54:36 +0530

    java day 5

---
+++ bca 3rd sem/java/method1.java
@@ -0,0 +1,30 @@
+public class method1 {
+    public static void pirntJAVA(){
+        System.out.println("Hello JAVA");
+    }
+
+    public static void printname(String name){
+        System.out.println(name);
+    }
+
+    public static void printsum(int a, int b){
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) { 
+        // METHODS
+
+        // System.out.println("Hello JAVA");
+        // System.out.println("Hello JAVA");
+        // System.out.println("Hello JAVA");
+
+        // pirntJAVA();
+        // pirntJAVA();
+        // pirntJAVA();
+
+        // printname("shivansh");
+
+        printsum(5,6);
+
+    }
+}
